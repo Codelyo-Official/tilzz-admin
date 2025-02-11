b222ba2c0e5d9b88cfc401be86f70e647caf0494
Adnan Latif <adnanlatif@Adnans-MacBook-Air.local>
2025-02-11 12:11:06 +0500

    changessss

--- src/components/Stories/stories.jsx
+++ src/components/Stories/stories.jsx
@@ -260,6 +260,100 @@ const FollowingStories = [
     }
 ]
 
+const AllStories = [
+    {
+        id:1,
+        title: "Hamza First Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description:"this is a story. And this is my description",
+        story_by_user:"john_doe",
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+    }, {
+        id:2,
+        title: "Hamza Second Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"hamza856",
+
+        follow: false,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    }, {
+        id:3,
+        title: "R6 Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"john_doe",
+
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    },{
+        id:4,
+        title: "Japan Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"john_doe",
+
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    }
+    ,{
+        id:5,
+        title: "Pak Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"john_doe",
+
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    },{
+        id:6,
+        title: "My Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"john_doe",
+
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    },{
+        id:7,
+        title: "Another Story",
+        img: "https://images.pexels.com/photos/3218465/pexels-photo-3218465.jpeg?auto=compress" +
+                "&cs=tinysrgb&w=1260&h=750&dpr=1",
+                description:"this is a story. And this is my description",
+                story_by_user:"john_doe",
+
+        follow: true,
+        liked:false,
+        like_count:10,
+        user_avatar:"https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1738868287~exp=1738871887~hmac=e24f4e7f6c2262238670c06cca214d2d0629465513fa6c63fdf54624c2855cf2&w=740"
+
+    }
+]
+
 function Stories({children,slugStories}) {
 
      const dispatch = useDispatch();
@@ -279,6 +373,9 @@ function Stories({children,slugStories}) {
         }else if(slugStories==="following-stories"){
             // api call for stories followed by the user
             setDataStories(FollowingStories);
+        }else if(slugStories==="public-feed"){
+            // api call for public feed stories
+            setDataStories(AllStories);
         }
     },[slugStories]);
 
@@ -296,8 +393,13 @@ function Stories({children,slugStories}) {
 
     return (
         <div>
-            <div className="logged-in-user-story-div">
-                <h2 className="heading-your-story">{slugStories===null || slugStories==="stories-feed" ? ("Stories"):slugStories==="my-stories"?"My Stories":"Following Stories"}</h2>
+            <div className="logged-in-user-story-div" style={{
+                backgroundColor: slugStories==="public-feed" ? "transparent":"white",
+                boxShadow:slugStories==="public-feed" ? "none":"rgba(149, 157, 165, 0.2) 0px 8px 24px",
+            }}>
+                {slugStories!=="public-feed" && (
+                    <h2 className="heading-your-story">{slugStories===null || slugStories==="stories-feed" ? ("Stories"):slugStories==="my-stories"?"My Stories":"Following Stories"}</h2>
+                )}
                 <div className="story-container">
                     <ul className="story-box101">
                         {dataStories.map((st, index) => {
--- src/pages/PublicStories/PublicStories.jsx
+++ src/pages/PublicStories/PublicStories.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import { FiArrowRightCircle } from "react-icons/fi";
 import "./publicstory.css";
 import HomeNavbar from "../../components/HomeNavbar/homenavbar";
+import Stories from "../../components/Stories/stories";
 
 function PublicStories() {
 
     return (
+        <>
         <HomeNavbar/>
+        <div>
+            <Stories slugStories={"public-feed"}/>
+        </div>
+        </>
     );
 }
 

3246248363cfd63513e49cce25bac632799a5090
Adnan Latif <adnanlatif@Adnans-MacBook-Air.local>
2025-02-13 10:47:04 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthProvider";
 import ProtectedRoute from "./components/ProtectedRoute";
-import "./App.css";
-// pages
-import Home from "./pages/Landing/Home";
-import NotFound from "./components/NotFound";
-import Login from "./pages/registeration/Login";
-import Register from "./pages/registeration/register";
-import Profile from "./pages/profile/Profile";
-import PublicStories from "./pages/PublicStories/PublicStories";
-
 //redux
 import { Provider } from 'react-redux';
 import { store } from './state/store';
+import "./App.css";
+
+// Lazy load pages
+const Home = lazy(() => import("./pages/Landing/Home"));
+const NotFound = lazy(() => import("./components/NotFound"));
+const Login = lazy(() => import("./pages/registeration/Login"));
+const Register = lazy(() => import("./pages/registeration/register"));
+const Profile = lazy(() => import("./pages/profile/Profile"));
+const PublicStories = lazy(() => import("./pages/PublicStories/PublicStories"));
 
 const App = () => {
   return (
     <AuthProvider>
       <Provider store={store}>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/stories-feed" element={<PublicStories/>} />
-            <Route path="/login" element={<ProtectedRoute><Login/></ProtectedRoute>} />
-            <Route path="/register" element={<ProtectedRoute><Register/></ProtectedRoute>} />
-            <Route path="/dashboard" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-            <Route path="*" element={<NotFound/>} />
-          </Routes>
+          {/* Suspense wraps the entire routing system to display a fallback UI during lazy loading */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/stories-feed" element={<PublicStories />} />
+              <Route path="/login" element={<ProtectedRoute><Login /></ProtectedRoute>} />
+              <Route path="/register" element={<ProtectedRoute><Register /></ProtectedRoute>} />
+              <Route path="/dashboard" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </Router>
       </Provider>
     </AuthProvider>
@@ -35,4 +38,3 @@ const App = () => {
 };
 
 export default App;
-

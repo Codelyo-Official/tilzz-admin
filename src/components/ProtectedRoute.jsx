3ca0fc372218e0b1f4a2accf0fff5bfc8eb1ddce
Hamza Latif <naqshband.hamza@gmail.com>
2025-03-22 03:30:19 +0500

    small stuff

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Navigate, useLocation} from "react-router-dom";
 import {useAuth} from "../contexts/AuthProvider";
 
+
 const ProtectedRoute = ({children}) => {
     const {getUser} = useAuth();
     const location = useLocation();

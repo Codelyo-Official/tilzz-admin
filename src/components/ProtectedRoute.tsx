8f71f0f5181388b41421fd37928f59637ffe0bfd
hamza <naqshband.hamza@gmail.com>
2025-04-16 22:35:01 +0500

    ok

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,15 +6,14 @@ import {useAuth} from "../contexts/AuthProvider";
 const ProtectedRoute = ({children}:{children:React.JSX.Element}) => {
     const {user} = useAuth();
     const location = useLocation();
-
-    // const user = getUser();
-
+    // console.log(location)
     if (location.pathname === "/dashboard" && user.username=="none") {
-        return <Navigate to="/login"/>;
+        return <Navigate to={`/login${location.search}`}/>;
     }
 
-    if ((location.pathname === "/login" || location.pathname === "/") && user.username!="none") {
-        return <Navigate to="/dashboard"/>;
+    if ((location.pathname === "/login" || location.pathname === "/register") && user.username!="none") {
+        // console.log("in here yo")
+        return <Navigate to={`/dashboard${location.search}`}/>;
     }
 
     return children;

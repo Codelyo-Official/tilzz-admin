2fc1af8fa807824d3b3a2cb9dd1c226078462cde
hamza <naqshband.hamza@gmail.com>
2025-06-07 17:51:27 +0500

    stuff

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,54 +1,65 @@
 import React from "react";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer,Area,Label } from "recharts";
 import styles from "./styles.module.css"
+import axios from "axios";
+import { ApiError } from "../../types/apiError";
 
 // Define the shape of each data point
 interface SignupData {
   date: string;
-  signups: number;
+  count: number;
 }
 
-// Generate dummy signup data for the last 30 days
-const generateLast30DaysData = (): SignupData[] => {
-  const data: SignupData[] = [];
-  for (let i = 29; i >= 0; i--) {
-    const date = new Date();
-    date.setDate(date.getDate() - i);
-    // const date = new Date();
-    const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-    const dd = String(date.getDate()).padStart(2, '0');
-    const mmdd = `${mm}-${dd}`;
-    data.push({
-      // date: date.toISOString().split("T")[0], // Format: YYYY-MM-DD
-      date:mmdd,
-      signups: Math.floor(Math.random() * 50) + 1, // Random number between 1 and 50
-    });
-  }
-  return data;
-};
-
-const data: SignupData[] = generateLast30DaysData();
-
+const API_BASE_URL = process.env.REACT_APP_BASE_URL;
 const Statistics: React.FC = () => {
+
+  const [signupdata,setSignupdata] = React.useState<SignupData[]>([]);
+  const getUserActivity = async () => {
+
+    try {
+      const token = sessionStorage.getItem('token');
+      const getUser_response = await axios.get(`${API_BASE_URL}/api/accounts/stats/user-activity/`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        }
+      });
+      console.log(getUser_response);
+      setSignupdata(getUser_response.data.signups);
+    } catch (err: any) {
+      console.log(err)
+      const apiError = err as ApiError;
+      if (apiError.response) {
+        const status = apiError.response.status;
+        const errorMessage = apiError.response.data?.error || 'Something went wrong on the server!';
+        alert(errorMessage);
+      }
+    } finally {
+    }
+
+  }
+
+  React.useEffect(()=>{
+    getUserActivity();
+  },[])
+
   return (
     <div className={styles.chartContainer} >
       <h2>User Signups (Last 30 Days)</h2>
       {/* width={100} height={500} */}
       <ResponsiveContainer width="100%" height="98%">
-        <LineChart data={data} className={styles.chart}
+        <LineChart data={signupdata} className={styles.chart}
          margin={{
           top: 10,
           right: 30,
-          left: 0,
+          left: 2,
           bottom: 10,
         }}
         >
-                    <CartesianGrid strokeDasharray="3 3" fill="rgb(249, 252, 255)" />
-
-          <Line type="monotone" dataKey="signups" stroke="rgb(54, 146, 238)" />
-          <XAxis dataKey="date" angle={0} textAnchor="end" height={50} style={{marginTop:"20px"}}>
+          <CartesianGrid strokeDasharray="3 3" fill="rgb(249, 252, 255)" />
+          <Line type="monotone" dataKey="count" stroke="rgb(54, 146, 238)" />
+          <XAxis dataKey="date" angle={0} textAnchor="end" height={50} style={{marginTop:"30px"}}>
             </XAxis>
-          <YAxis />
+          <YAxis dataKey={"count"} />
           <Tooltip />
         </LineChart>
       </ResponsiveContainer>
